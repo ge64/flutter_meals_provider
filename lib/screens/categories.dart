b9e0a418b14b775d8426a9ac63a9094f4e2c7728
ge64 <eatongn@gmail.com>
2024-07-25 15:59:13 -0400

    passing data to target screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
 
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/models/category.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 import 'package:meals/screens/meals.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectCategory(BuildContext context) {
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals.where((meal) => meal.categories.contains(category.id)).toList();
+
     // Navigator.push(context, route); // This is another way to do the same thing in this context
     Navigator.of(context).push(
       MaterialPageRoute(
         builder: (ctx) => MealsScreen(
-          title: 'Some title',
-          meals: [],
+          title: category.title,
+          meals: filteredMeals,
         ),
       ),
     );
@@ -37,9 +40,12 @@ class CategoriesScreen extends StatelessWidget {
           // alternative:
           // availableCategories.map((category) => CategoryGridItem(category: category)).toList()
           for (final category in availableCategories)
-            CategoryGridItem(category: category, onSelectCategory: () {
-              _selectCategory(context);
-            },)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              },
+            )
         ],
       ),
     );
